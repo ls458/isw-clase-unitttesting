9a8196ae765c78e468f6ea23195cd8d7c5ebc357
Emiliano Reynares <reynares.emiliano@gmail.com>
2019-11-06 13:04:17 -0300

    Code reorganized. Logic and test in separate files.

---
+++ scicalc.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python
+#
+# Emiliano Reynares <reynares.emiliano@gmail.com>
+
+class SciCalc():
+    def inverseOfLogX(self, x):
+        return 1/log(x, 10)
---
+++ tests.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+#
+# Emiliano Reynares <reynares.emiliano@gmail.com>
+
+from math import log
+import unittest
+from scicalc import SciCalc
+
+class TestLog(unittest.TestCase):
+    def test_1(self):
+        calc = SciCalc()
+        r = calc.inverseOfLogX(10)
+        self.assertEqual(1.5, r)
+
+    def test_zerodivision(self):
+             with self.assertRaises(ZeroDivisionError):
+                 calc = SciCalc()
+                 r = calc.inverseOfLogX(1)
+
+if __name__ == '__main__':
+    unittest.main()
